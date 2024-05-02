61f4dd8aaa110622d36a2efd8dfbb466e829968d
mn-48 <nazmul.cse48@gmail.com>
2024-05-02 13:11:18 +0600

    PHONENUMBER_DEFAULT_REGION

--- users/admin.py
+++ users/admin.py
@@ -3,11 +3,13 @@ from .models import User
 from django import forms
 from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
+
 class UserForm(forms.ModelForm):
     class Meta:
         widgets = {                          # Here
             'phone': PhoneNumberPrefixWidget(initial='BD'),
         }
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
